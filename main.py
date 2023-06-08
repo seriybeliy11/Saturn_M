b7c6a97c1b093995caa367529ebfb4560fe0e62c
RGB-Grey-Chrome <129196368+seriybeliy11@users.noreply.github.com>
2023-06-08 19:29:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,43 @@
-import subprocess
-subprocess.check_call(['pip', 'install', '-r', 'requirements.txt'])
-
-from SaturiC import *
+import os
+import sys
+import time
 import webbrowser
+from SaturiC import *
+
+
+def install_requirements(requirements_file):
+    os.system(f"{sys.executable} -m pip install -r {requirements_file}")
 
 
 def main():
-    subprocess.run(['python', 'csv_g.py'])
-    subprocess.check_call(['pip', 'install', '-r', 'requirements.txt'])
-    while True:
-        time.sleep(7)
-        print('----------_--- -- - -- - -- -      - - - - - -- ---- -- -- -  -')
-        print('- - -  --   ---    ---      --  -- - ------- ---- ---  --- ----')
-        print('Hello! You are using the SaturiC Analyze System')
-        print('----------------------------------------------------------------')
-        print('- - -  --   ---    ---      --  -- - ------- ---- ---  --- -----')
-        print('----------------------------------------------------------------')
-        print('- - -  --   ---    ---      --  -- - ------- ---- ---  --- -----')
-        get_contributors()
-        print('About issues:')
-        get_issues()
-        get_pulls()
-        average_time_issue()
-        get_AVG()
-        get_KPI_pulls()
-        get_KPI_contribute()
-        print('About timelines:')
-        #get_time_ad()
-        print('About commenters:')
-        get_commenters()
-        break
+    install_requirements('requirements.txt')
 
-    subprocess.call(['python', 'app.py'])
+    os.system('python csv_g.py')
+
+    time.sleep(7)
+    print('--------------------------------------------')
+    print('Hello! You are using the SaturiC Analyze System')
+    print('--------------------------------------------')
+
+    get_contributors()
+    print('About issues:')
+    get_issues()
+    get_pulls()
+    average_time_issue()
+    get_AVG()
+    get_KPI_pulls()
+    get_KPI_contribute()
+    print('About timelines:')
+    # get_time_ad()
+    print('About commenters:')
+    get_commenters()
+
+    os.system('python dts.py')
+    os.system('python app.py')
+
     url = 'http://127.0.0.1:8050/'
     webbrowser.open_new_tab(url)
+
 
 if __name__ == "__main__":
     main()
