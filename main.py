7d6410b22abc4a1d8fe5a6d6c16037e7caeb8a22
RGB-Grey-Chrome <129196368+seriybeliy11@users.noreply.github.com>
2023-06-08 19:39:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import webbrowser
+import platform
 from SaturiC import *
 
 
@@ -9,10 +10,20 @@ def install_requirements(requirements_file):
     os.system(f"{sys.executable} -m pip install -r {requirements_file}")
 
 
+def run_command(command):
+    if platform.system() == 'Windows':
+        os.system(command)
+    elif platform.system() == 'Darwin':  # Для macOS
+        os.system(f"python3 {command}")
+    else:
+        print("Unsupported operating system.")
+        sys.exit(1)
+
+
 def main():
     install_requirements('requirements.txt')
 
-    os.system('python csv_g.py')
+    run_command('csv_g.py')
 
     time.sleep(7)
     print('--------------------------------------------')
@@ -32,8 +43,8 @@ def main():
     print('About commenters:')
     get_commenters()
 
-    os.system('python dts.py')
-    os.system('python app.py')
+    run_command('dts.py')
+    run_command('app.py')
 
     url = 'http://127.0.0.1:8050/'
     webbrowser.open_new_tab(url)
