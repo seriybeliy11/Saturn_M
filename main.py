61621adee5661ab003a96530974a06506cb327fd
RGB-Grey-Chrome <129196368+seriybeliy11@users.noreply.github.com>
2023-06-10 13:55:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import time
 import webbrowser
 import platform
 from SaturiC import *
+from flask import Flask
+
+app = Flask(__name__)
 
 
 def install_requirements(requirements_file):
@@ -20,12 +23,34 @@ def run_command(command):
         sys.exit(1)
 
 
+@app.route('/bpdn')
+def app1():
+    run_command('barplots_perm_duration_and_unique_commenters.py')
+    return 'Barplots Perm Duration and Unique Commenters executed.'
+
+
+@app.route('/moneyQ')
+def app3():
+    run_command('moneq.py')
+    return 'MoneyQ executed.'
+
+
+@app.route('/closedplanned')
+def app4():
+    run_command('closed_not_planned.py')
+    return 'Closed Not Planned executed.'
+
+
+@app.route('/DTS')
+def app5():
+    run_command('dts.py')
+    return 'DTS executed.'
+
+
 def main():
     install_requirements('requirements.txt')
 
     run_command('csv_g.py')
-
-    time.sleep(7)
     print('--------------------------------------------')
     print('Hello! You are using the SaturiC Analyze System')
     print('--------------------------------------------')
@@ -43,11 +68,7 @@ def main():
     print('About commenters:')
     get_commenters()
 
-    run_command('dts.py')
-    run_command('app.py')
-
-    url = 'http://127.0.0.1:8050/'
-    webbrowser.open_new_tab(url)
+    app.run()
 
 
 if __name__ == "__main__":
